d2e98e182fad4c029a9e146fb1ba903982eaf3c9
DThemse <de.themse@gmail.com>
2024-05-29 18:44:23 +0300

    feat: internal api abstraction

--- src/services/api/internal/posts.ts
+++ src/services/api/internal/posts.ts
@@ -1,5 +1,6 @@
 import { internalClient } from '@/services/libs/internalClient';
 import { Post } from '@/types/models/Post';
+import { generateUuid } from '@/utils/generateUuid';
 
 const url = '/posts';
 
@@ -11,10 +12,42 @@ export async function findAll(_criteria?: unknown) {
 	return data;
 }
 
-export async function findById() {}
+export async function findById(id: string) {
+	const { data } = await internalClient.get<Post>(`${url}/${id}`);
 
-export async function create() {}
+	return data;
+}
 
-export async function update() {}
+export async function create(input: Omit<Post, 'id'>) {
+	const { data } = await internalClient.post<Post>(url, {
+		...input,
+		id: generateUuid(),
+	});
 
-export async function deleteItem() {}
+	return data;
+}
+
+export async function update(id: string, input: Partial<Omit<Post, 'id'>>) {
+	const { id: postId, ...post } = await findById(id);
+
+	if (!post) {
+		throw new Error('Post not found');
+	}
+
+	const { data } = await internalClient.put<Post>(`${url}/${id}`, {
+		...post,
+		...input,
+	});
+
+	return data;
+}
+
+export async function deleteItem(id: string) {
+	const { id: postId, ...post } = await findById(id);
+
+	if (!post) {
+		throw new Error('Post not found');
+	}
+
+	await internalClient.delete(`${url}/${postId}`);
+}
